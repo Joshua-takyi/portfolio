a0c6cf8340fc272acefd812e2bbd0a021b049b24
joshua Takyi <takyijoshua019@gmail.com>
2024-05-31 18:42:42 +0000

    almost

--- src/components/myUi/heroSection.jsx
+++ src/components/myUi/heroSection.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
 	return (
 		<main className="container mx-auto flex lg:flex-row flex-col lg:mt-20 mt-5 lg:space-y-0 space-y-7 lg:justify-between lg:p-5">
 			<div className="lg:w-1/2 flex flex-col lg:p-4 lg:space-y-3 items-center lg:items-start space-y-5 justify-center">
-				<h1 className=" font-extrabold lg:text-6xl text-balance text-[1.8rem] md:text-4xl leading-[37px] text-center md:leading-[45px] lg:text-left">
+				<h1 className=" font-extrabold lg:text-[55px] text-balance text-[1.8rem] md:text-4xl leading-[37px] text-center md:leading-[45px] lg:text-left">
 					The most reliable&nbsp;&
 					<span className="text-primary">
 						&#160; comprehensive Cryptocurrency
@@ -19,7 +19,7 @@ export default function Hero() {
 			</div>
 			<div className="lg:w-1/2 flex flex-row justify-center items-start">
 				<Image
-					src="/hidden.gif"
+					src="/monetization.gif"
 					width={500}
 					height={500}
 					alt="Bitcoin"
